7e882b1d8977177ee85e2dc1369273aaef9e8fc9
Kescefko <markgyuro@gmail.com>
2024-12-18 10:16:58 +0100

    fixed scope typo and added logic to set context for the subscription

--- rbac/set_rbac_vault.sh
+++ rbac/set_rbac_vault.sh
@@ -6,8 +6,11 @@ read -p "Enter Key Vault Name: " keyvault_name
 read -p "Enter User Object ID: " object_id
 read -p "Enter Role (e.g., Key Vault Administrator, Key Vault Secrets User): " role
 
+# set context
+az account set -s $subscription_id
+
 # Grant the specified role to the user
 az role assignment create \
     --role "$role" \
-    --scope "/subscriptions/${subscription_id}/resourceGroups/${resource_group_name}/providers/Microsoft.KeyVault/vaults/${keyvault_name}" \
+    --scope "subscriptions/${subscription_id}/resourceGroups/${resource_group_name}/providers/Microsoft.KeyVault/vaults/${keyvault_name}" \
     --assignee-object-id "${object_id}"
