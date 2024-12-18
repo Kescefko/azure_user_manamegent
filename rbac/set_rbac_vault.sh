32d2c8d92f74138e12ecd9737b2bd0aaa7c6dc52
Kescefko <markgyuro@gmail.com>
2024-12-18 10:10:37 +0100

    add script for setting rbac on key vault

---
+++ rbac/set_rbac_vault.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+read -p "Enter Subscription ID: " subscription_id
+read -p "Enter Resource Group Name: " resource_group_name
+read -p "Enter Key Vault Name: " keyvault_name
+read -p "Enter User Object ID: " object_id
+read -p "Enter Role (e.g., Key Vault Administrator, Key Vault Secrets User): " role
+
+# Grant the specified role to the user
+az role assignment create \
+    --role "$role" \
+    --scope "/subscriptions/${subscription_id}/resourceGroups/${resource_group_name}/providers/Microsoft.KeyVault/vaults/${keyvault_name}" \
+    --assignee-object-id "${object_id}"
